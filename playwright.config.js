5b2a723bcbeba1623c674bf8e100af116e9d82d8
daniellepatricio <dpatricio@brainworks.com>
2026-06-13 23:28:17 -0300

    ci: alteração config report

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', { open: false }],
+    ['html', { open: true }],
     ['junit', { outputFile: `results.xml` }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
